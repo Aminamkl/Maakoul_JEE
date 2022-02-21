4a4af2bbfa272170f1c53630b14ca77ffa43cdb4
Amina Maakoul <maakoulamina@gmail.com>
2022-02-21 16:40:02 +0100

    Test prg2

--- 2-Prg-ioc2/src/test/java/metier/CalculTest.java
+++ 2-Prg-ioc2/src/test/java/metier/CalculTest.java
@@ -1,9 +1,12 @@
 package metier;
 
 import org.junit.Assert;
+import org.junit.Test;
+
 
 public class CalculTest {
     private Calcul calcul;
+    @Test
     public void testSomme(){
         calcul=new Calcul();
         double a=5; double b=9;
